5ac45aa54a96498b852f83ca9afe337767aa1de4
Ian Ansell <ian.ansell.uk@gmail.com>
2020-03-25 10:14:51 +0000

    gtm.py finished with get_gtm() fn

--- src/gtm.py
+++ src/gtm.py
@@ -7,37 +7,36 @@ import os, sys, logging, json
 import report
 import auth
 
-try:
+
+def get_gtm(var_id):
     key_file = auth.auth("cj_data")
     SCOPES = ['https://www.googleapis.com/auth/tagmanager.readonly']
     credentials = ServiceAccountCredentials.from_json_keyfile_dict(key_file, SCOPES)
     tag_manager =  build('tagmanager', 'v2', credentials=credentials)
 
-    mouseflow = "accounts/1090055821/containers/6311504/workspaces/1000189/variables/74"
+    path = "accounts/1090055821/containers/6311504/workspaces/1000189/variables/"+var_id
     frame = pd.DataFrame()
-except:
-    print("An exception occurred importing gtm.py")
 
+    variable = tag_manager.accounts().containers().workspaces().variables().get(
+        path=path
+        ).execute()
 
-variable = tag_manager.accounts().containers().workspaces().variables().get(
-    path=mouseflow
-    ).execute()
+    para_list = variable["parameter"]
+    x = next(item for item in para_list if item["key"] == "map")
+    y = x["list"]
 
-para_list = variable["parameter"]
-x = next(item for item in para_list if item["key"] == "map")
-y = x["list"]
+    for item in y:
+        url = item["map"][0]["value"]
+        value = item["map"][1]["value"]
 
+        row = {
+        "url": url,
+        "value": value
+        }
 
-for item in y:
-    url = item["map"][0]["value"]
-    value = item["map"][1]["value"]
+        frame = frame.append(row,ignore_index = True)
 
-    row = {
-    "url": url,
-    "value": value
-    }
+    return frame
 
-    frame = frame.append(row,ignore_index = True)
-
-
-return frame
+if __name__ == '__main__':
+    get_gtm("74")
